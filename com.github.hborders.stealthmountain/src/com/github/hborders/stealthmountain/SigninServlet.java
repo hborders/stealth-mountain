f3dacbf1f3c65bba88cb741eb09b8735af40ffed
hborders <hborders@mail.win.org>
2012-04-30 23:25:09 -0500

    Implemented basic twitter signin/tweet/signout. Need to improve storage before moving to prod.

---
+++ com.github.hborders.stealthmountain/src/com/github/hborders/stealthmountain/SigninServlet.java
@@ -0,0 +1,40 @@
+package com.github.hborders.stealthmountain;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.RequestToken;
+
+@SuppressWarnings("serial")
+public class SigninServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		Twitter twitter = new TwitterFactory().getInstance();
+		// Fill in these values below, not committing to github for security
+		// reasons
+		twitter.setOAuthConsumer("consumer key", "consumer secret");
+		request.getSession().setAttribute("twitter", twitter);
+		try {
+			StringBuffer callbackUrlStringBuffer = request.getRequestURL();
+			int index = callbackUrlStringBuffer.lastIndexOf("/");
+			callbackUrlStringBuffer.replace(index,
+					callbackUrlStringBuffer.length(), "").append("/callback");
+
+			RequestToken requestToken = twitter
+					.getOAuthRequestToken(callbackUrlStringBuffer.toString());
+			request.getSession().setAttribute("requestToken", requestToken);
+			response.sendRedirect(requestToken.getAuthenticationURL());
+		} catch (TwitterException e) {
+			throw new ServletException(e);
+		}
+	}
+}
---
+++ com.github.hborders.stealthmountain/src/com/github/hborders/stealthmountain/SignoutServlet.java
@@ -0,0 +1,18 @@
+package com.github.hborders.stealthmountain;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class SignoutServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().invalidate();
+		response.sendRedirect(request.getContextPath() + "/tweet.html");
+	}
+}
